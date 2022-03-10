beb1ba9e11dc2ada9c11f5550fed274efb8e9842
koda-ma <r.kodama.sys20@morijyobi.ac.jp>
2022-03-10 11:21:42 +0900

    [Add] create_group.htmlの作成、renderする処理の追加

--- create_group.py
+++ create_group.py
@@ -1,3 +1,10 @@
 from flask import Flask, Blueprint, render_template, redirect, request, url_for, session
 
 create_group = Blueprint('create_group', __name__, url_prefix='/create-group')
+
+
+@create_group.route("/", methods=['GET'])
+def create_group_form():
+    # if "user_id" not in session:  # セッションの有無
+    #     return redirect("/")
+    return render_template("create_group.html")
