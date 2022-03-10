5133fa78fea575ea2add0eafca2703ca45217379
koda-ma <r.kodama.sys20@morijyobi.ac.jp>
2022-03-10 11:47:58 +0900

    [Add] GETとPOSTで処理を分ける記述に変更
    html側で表示し、フォームの内容が受け取れていることを確認

--- create_group.py
+++ create_group.py
@@ -3,8 +3,17 @@ from flask import Flask, Blueprint, render_template, redirect, request, url_for,
 create_group = Blueprint('create_group', __name__, url_prefix='/create-group')
 
 
-@create_group.route("/", methods=['GET'])
+@create_group.route("/", methods=['GET', 'POST'])
 def create_group_form():
     # if "user_id" not in session:  # セッションの有無
-    #     return redirect("/")
-    return render_template("create_group.html")
+    # return redirect("/")
+    if request.method == 'GET':
+        return render_template("create_group.html")
+    elif request.method == 'POST':
+        group_name = request.form.get('group_name')
+        if group_name is None or group_name == '':  # グループ名が入力されていなかった場合
+            message = 'グループ名が入力されていません'
+            return render_template('create_group.html', alert_message=message)
+        else:
+            message = "グループ名: " + group_name
+            return render_template('create_group.html', alert_message=message)
