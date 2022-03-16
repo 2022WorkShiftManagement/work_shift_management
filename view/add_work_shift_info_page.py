a03ce1894fcc3dd1974c2cfa97f27a9892998bba
ゆうと <67322696+matico62@users.noreply.github.com>
2022-03-17 00:20:32 +0900

    ログインをしているかの判定を追加

--- view/add_work_shift_info_page.py
+++ view/add_work_shift_info_page.py
@@ -7,58 +7,67 @@ add_work_shift_info = Blueprint('add_work_shift_info', __name__)
 # バイト先情報登録画面
 @add_work_shift_info.route("/add_work_shift_info")
 def add_work_shift_info_page():
-    error = request.args.get('error')
-    return render_template(
-        'add_work_shift_info.html',
-        color=get_color(),
-        error=error
-    )
+    if "user" not in session:
+        return redirect("/")
+    else:
+        error = request.args.get('error')
+        return render_template(
+            'add_work_shift_info.html',
+            color=get_color(),
+            error=error
+        )
 
 
 # バイト先情報登録確認画面
 @add_work_shift_info.route("/confirm_work_shift_info", methods=["POST"])
 def confirm_work_shift_info_page():
-    job_name = request.form.get('job_name')
-    job_color = request.form.get('color')
+    if "user" not in session:
+        return redirect("/")
+    else:
+        job_name = request.form.get('job_name')
+        job_color = request.form.get('color')
 
-    # 入力チェック
-    if job_name and job_color:
-        session['job'] = {
-            'name': job_name,
-            'color_code': job_color,
-            'color_name': get_color(job_color)
-        }
+        # 入力チェック
+        if job_name and job_color:
+            session['job'] = {
+                'name': job_name,
+                'color_code': job_color,
+                'color_name': get_color(job_color)
+            }
 
-        return render_template('confirm_work_shift_info.html')
+            return render_template('confirm_work_shift_info.html')
 
-    # 入力に不備があったら入力画面へリダイレクト
-    return redirect(
-        url_for(
-            'add_work_shift_info.add_work_shift_info_page',
-            error='入力内容に不備があります。'
+        # 入力に不備があったら入力画面へリダイレクト
+        return redirect(
+            url_for(
+                'add_work_shift_info.add_work_shift_info_page',
+                error='入力内容に不備があります。'
+            )
         )
-    )
 
 
 # バイト先情報登録
 @add_work_shift_info.route("/insert_work_shift_info")
 def insert_work_shift_info():
-    insert_result = add_work_shift_info_db.insert_work_shift_info(
-        session['user'],
-        session['job']['name'],
-        session['job']['color_code']
-    )
-
-    # データベースに登録できたか
-    if insert_result:
-        return redirect("/home")
-
-    return redirect(
-        url_for(
-            'add_work_shift_info.add_work_shift_info_page',
-            error='登録に失敗しました。'
+    if "user" not in session:
+        return redirect("/")
+    else:
+        insert_result = add_work_shift_info_db.insert_work_shift_info(
+            session['user'],
+            session['job']['name'],
+            session['job']['color_code']
         )
-    )
+
+        # データベースに登録できたか
+        if insert_result:
+            return redirect("/home")
+    
+        return redirect(
+            url_for(
+                'add_work_shift_info.add_work_shift_info_page',
+                error='登録に失敗しました。'
+            )
+        )
 
 
 # シフト表示画面（仮）
