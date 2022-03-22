c1b9c220b8a6c292f169d63d872db348b38fdd73
tiger <t.komatsu.sys20@morijyobi.ac.jp>
2022-03-22 14:54:47 +0900

    バリデーションチェックの強化
    ・正規表現チェックを強化しました
    ・htmlのenをjaに変更

--- view/login.py
+++ view/login.py
@@ -1,20 +1,33 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from db import login_db
+from function import user_function as uf
 
 login = Blueprint('login', __name__)
 
 
 # ログインページ
-@login.route("/top", methods=["POST"])
-def top_page():
+@login.route("/home_post", methods=["POST"])
+def home_post_page():
     mail = request.form.get('mail')
     pw = request.form.get('pw')
 
-    user_id = login_db.login(mail, pw)
-    print(user_id)
+    if uf.mail_validation(mail) and uf.pw_validation(pw):
+        user_id = login_db.login(mail, pw)
+    else:
+        return redirect('/')
+
     if user_id is None:
         return redirect("/")
 
     session['user'] = user_id
 
     return render_template('home.html')
+
+
+# sessionが残っていれば直でhome.htmlに遷移
+@login.route("/home_get", methods=["GET"])
+def home_get_page():
+    if 'user' in session:
+        return render_template('home.html')
+
+    return redirect('/home_post')
