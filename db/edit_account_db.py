c2e160f17d17818d80035eb29ece496bbad4a75c
ゆうと <67322696+matico62@users.noreply.github.com>
2022-03-23 11:51:08 +0900

    アカウントの編集機能の作成

--- db/edit_account_db.py
+++ db/edit_account_db.py
@@ -8,16 +8,32 @@ from db import connect_db
 
 
 # アカウントの取得
-def search_account(user_id):
+def search_account(user_id, pw=None):
     conn = connect_db.get_select_connection()
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
 
-    sql = 'select user_id, mail, user_name from users where user_id = %s'
+    if pw:
+        salt = search_salt(user_id)
 
-    try:
-        cur.execute(sql, (user_id,))
-    except Exception as e:
-        return False
+        if salt is None:
+            return False
+
+        # ハッシュ化の関数
+        hashed_pw = user_function.hash(salt, pw)
+
+        sql = 'select user_id, mail, user_name from users where user_id = %s AND password = %s'
+
+        try:
+            cur.execute(sql, (user_id, hashed_pw))
+        except Exception as e:
+            return False
+    else:
+        sql = 'select user_id, mail, user_name from users where user_id = %s'
+
+        try:
+            cur.execute(sql, (user_id,))
+        except Exception as e:
+            return False
 
     account = cur.fetchone()
 
@@ -25,6 +41,26 @@ def search_account(user_id):
     conn.close()
 
     return account
+
+
+# ソルトの取得
+def search_salt(user_id):
+    conn = connect_db.get_select_connection()
+    cur = conn.cursor()
+
+    sql = 'select salt from users where user_id = %s'
+
+    try:
+        cur.execute(sql, (user_id,))
+    except Exception as e:
+        return False
+
+    salt = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return salt[0]
 
 
 # アカウントの編集
@@ -35,10 +71,10 @@ def update_account(user_id, user_name, pw):
     conn = connect_db.get_update_connection()
     cur = conn.cursor()
 
-    sql = "UPDATE job_infos SET user_name = %s, password = %s WHERE user_id = %s"
+    sql = "UPDATE users SET user_name = %s, password = %s, salt = %s WHERE user_id = %s"
 
     try:
-        cur.execute(sql, (user_name, hashed_pw, user_id))
+        cur.execute(sql, (user_name, hashed_pw, salt, user_id))
     except Exception as e:
         return False
 
