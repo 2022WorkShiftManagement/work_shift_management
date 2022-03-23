4faac9a4cb169b873f468365d80cdcccb3678ea7
ゆうと <67322696+matico62@users.noreply.github.com>
2022-03-24 03:10:09 +0900

    バイト先関連のurlを変更

--- db/job_db.py
+++ db/job_db.py
@@ -1,6 +1,24 @@
 import MySQLdb
-
 from db import connect_db
+
+
+# バイト先情報を登録
+def insert_work_shift_info(user_id, job_name, color):
+    conn = connect_db.get_update_connection()
+    cur = conn.cursor()
+
+    sql = "INSERT INTO job_infos(user_id, job_name, color_code) VALUES(%s, %s, %s)"
+
+    try:
+        cur.execute(sql, (user_id, job_name, int(color, 16)))
+    except Exception as e:
+        return False
+
+    cur.close()
+    conn.commit()
+    conn.close()
+
+    return True
 
 
 # ユーザの登録しているバイト先を取得
